bd28c7c71e70d40068dbce4c6f31a23dbfaa8810
Hanu <iknowaditya16@gmail.com>
2022-05-22 15:14:50 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY>500){
+            $('.scroll-up-btn').addClass("show");
+        }
+        else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+
+    var typed=new Typed(".typing", {
+        strings: ["Developer", "Freelancer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    var typed=new Typed(".typing-2", {
+        strings: ["Developer", "Freelancer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                item: 1,
+                nav: false
+            },
+            600:{
+                item: 2,
+                nav: false
+            },
+            1000:{
+                item: 3,
+                nav: false
+            },
+        }
+
+    });
+});
